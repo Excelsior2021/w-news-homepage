715adc4cf8f40d0b902229ee34ca12e570f336b5
Jonathan Kila <temidee11@yahoo.co.uk>
2023-02-19 12:14:00 +0000

    ready to submit

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+const body = document.getElementById("body")
 const menu = document.getElementById("menu")
 const closeMenu = document.getElementById("close-menu")
 const modal = document.getElementById("modal")
+const modalLinks = document.getElementsByClassName("modal__link")
 
-menu.addEventListener("click", () => modal.classList.add("modal--show"))
+const closeModal = () => {
+  modal.classList.remove("modal--show")
+  body.style.overflowY = "auto"
+}
 
-closeMenu.addEventListener("click", () => modal.classList.remove("modal--show"))
+menu.addEventListener("click", () => {
+  modal.classList.add("modal--show")
+  body.style.overflowY = "hidden"
+})
+
+closeMenu.addEventListener("click", () => closeModal())
+
+for (const link of modalLinks) {
+  link.addEventListener("click", () => closeModal())
+}
